fca49d64f704c765c17ad7e1ddcb6d5533af7211
mariel-delagarza <mariel.code@gmail.com>
2020-05-16 21:06:54 -0400

    Set up variables for the gird, squares, and score button/display

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  // Use query selector to look through HTML and find
+  // className of grid, and assign it to our constant
+  const grid = document.querySelector('.grid')
+
+  // Turn all 200 divs into an array we can work with
+  // and give them a specific index number
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+
+  // A hash indicates we are looking for an id
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
+
+  const width = 10;
 
 
-  
 })
